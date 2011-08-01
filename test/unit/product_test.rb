c8867f21c7da00282f067e11bb141b5c377ebf09
Glenn Martin <glennmartin76@gmail.com>
2011-08-01 22:33:14 +1000

    001

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -3,7 +3,7 @@
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.   reree    tested
+# We make no guarantees that this code is fit for any purpose.   reree    tested  wassupening
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 require 'test_helper'
